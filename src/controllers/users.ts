69bbed6735335a8fce26f76d7909ea058749e6e6
Mayan <gabriel.mayan@gafit.com.br>
2023-06-15 13:01:37 -0300

    Correções de código e ajustes no eslint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,9 @@
-import { getUsers } from '../repositories/UserRepository';
+import { UserRepository } from '../entity/User';
 import { Request, Response } from 'express';
 
 export const getUser = async (request: Request, response: Response) => {
   try {
-    const teste = await getUsers();
+    const teste = await UserRepository.getUsers();
     console.log(teste)
     return response.status(200).json(teste);
   } catch (error: any) {
