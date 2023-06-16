7a52e20346c006a78c5c1693fa23357a601ec054
Mayan <gabriel.mayan@gafit.com.br>
2023-06-16 10:31:20 -0300

    Refatoração nas validações

--- src/validations/userSchema.ts
+++ src/validations/userSchema.ts
@@ -1,16 +1,31 @@
-import yup from '../services/yup';
+import yup from "@services/yup";
 
 export const createUserSchema = yup.object().shape({
   userName: yup
     .string()
     .strict()
     .required(),
-    //.test('equal', 'Invalid user name', async (userName) => testUserName(userName)),
+  // .test('equal', 'Invalid user name', async (userName) => testUserName(userName)),
 
   password: yup
     .string()
     .strict()
     .required()
-    .min(8, 'A senha deve ter pelo menos 8 caracteres'),
-    //.test('equal', 'A senha deve conter uma combinação de letras, números e símbolos', (password) => testPassword(password)),
+    .min(8, "A senha deve ter pelo menos 8 caracteres"),
+  //  .test('equal', 'A senha deve ter letras, números e símbolos', (pwd) => testPassword(pwd)),
+});
+
+export const createAdminSchema = yup.object().shape({
+  userName: yup
+    .string()
+    .strict()
+    .required(),
+  // .test('equal', 'Invalid user name', async (userName) => testUserName(userName)),
+
+  password: yup
+    .string()
+    .strict()
+    .required()
+    .min(8, "A senha deve ter pelo menos 8 caracteres"),
+  //  .test('equal', 'A senha deve ter letras, números e símbolos', (pwd) => testPassword(pwd)),
 });
